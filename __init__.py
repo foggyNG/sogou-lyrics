7659a0c0292bdb42073df6f6507e7fd08ecf2501
gogo.wonder <gogo.wonder@15cf7538-5204-11de-9a16-8934cb1294a2>
2009-06-05 19:12:48 +0000

    first commit, base functionality

---
+++ __init__.py
@@ -0,0 +1,199 @@
+import os, sys, ClientCookie, urllib2, re
+import rhythmdb, rb
+from gnomeosd import eventbridge
+
+# global settings
+LRCDIR = os.path.expanduser('~/.lyrics')
+TOKEN_STRIP = ['\([^\)]*\)',' ']
+
+ANIMATIONS = 'off'
+SHADOW = 'off'
+HALIGNMENT = 'center'
+VPOSITION = 'top'
+FOREGOUND = 'yellow'
+TRANSLUCENT = 'off'
+TIMEOUT = 20000
+SIZE = 20000
+MESSAGE_TEMPLATE = "<message id='SogouLyrics' animations='%s' osd_fake_translucent_bg='%s' drop_shadow='%s' osd_vposition='%s' osd_halignment='%s'  hide_timeout='%d'><span size='%d' foreground='%s'>%%s</span></message>" % (ANIMATIONS, TRANSLUCENT, SHADOW, VPOSITION, HALIGNMENT, TIMEOUT, SIZE, FOREGOUND)
+
+def detect_charset(s):
+	charsets = ('iso-8859-1', 'gbk', 'utf-8')
+	for charset in charsets:
+		try:
+			return unicode(unicode(s, 'utf-8').encode(charset), 'gbk')
+		except:
+			continue
+	return s
+
+def parse_lyrics(lines):
+	content = {}
+	re_ti = re.compile('\[ti:[^\]]*\]')
+	re_ar = re.compile('\[ar:[^\]]*\]')
+	re_offset = re.compile('\[offset:[^\]]*\]')
+	re_lrc = re.compile('(\[[0-9\.:]*\])+.*')
+	re_time = re.compile('\[[0-9]{2}:[0-9]{2}\.[0-9]{2}\]')
+	for line in lines:
+		# search for title property
+		m = re_ti.search(line)
+		if not m is None:
+			segment = m.group(0)
+			content['ti'] = segment[4:-1]
+		# search for artist property
+		m = re_ar.search(line)
+		if not m is None:
+			segment = m.group(0)
+			content['ar'] = segment[4:-1]
+		# search for offset property
+		m = re_offset.search(line)
+		if not m is None:
+			segment = m.group(0)
+			content['offset'] = int(segment[8:-1])/1000
+		# parse lrc
+		m = re_lrc.match(line)
+		if not m is None:
+			pos = 0
+			tm = re_time.findall(line)
+			for time in tm:
+				pos = pos + len(time)
+			lrc = m.group(0)[pos:]
+			for time in tm:
+				try:
+					minute = int(time[1:3])
+					second = int(time[4:6])
+					key = minute * 60 + second
+					content[key] = lrc
+				except ValueError:
+					print 'parse timestamp error %s' % time
+	return content
+
+def verify_lyrics(content, artist, title):
+	if not content.has_key('ar') or not content.has_key('ti'):
+		# artist or title not found in lyrics file
+		return 0
+	else:
+		ar = content['ar']
+		ti = content['ti']
+		print 'check lrc (%s - %s, %s - %s)' % (artist, title, ar, ti)
+		ar = ar.lower()
+		ti = ti.lower()
+		ar1 = artist.lower()
+		ti1 = title.lower()
+		for strip in TOKEN_STRIP:
+			ar1 = re.sub(strip, '', ar1)
+			ti1 = re.sub(strip, '', ti1)
+		if ar.find(ar1) != -1 and ti.find(ti1) != -1:
+			return 1
+		else:
+			return 0
+			
+def download_lyrics(artist, title):
+	# grab song search page
+	title_encode = urllib2.quote(detect_charset(title).encode('gbk').replace(' ', ''))
+	artist_encode = urllib2.quote(detect_charset(artist).encode('gbk').replace(' ',''))
+	uri = 'http://mp3.sogou.com/music.so?query=%s%%20%s' % (artist_encode, title_encode)
+	print 'search page <%s>' % uri
+	cache = ClientCookie.urlopen(ClientCookie.Request(uri)).readlines()
+	for line in cache:
+		# grab lyrics search page, only use the first
+		m = re.search('geci\.so\?[^\"]*', line.decode('gbk'))
+		if not m is None:
+			uri = 'http://mp3.sogou.com/%s' % m.group(0)
+			print 'lrc page <%s>' % uri
+			cache = ClientCookie.urlopen(ClientCookie.Request(uri)).readlines()
+			for line in cache:
+				# grab lyrics file uri, try all of them
+				m = re.search('downlrc\.jsp\?[^\"]*', line.decode('gbk'))
+				if not m is None:				
+					uri = 'http://mp3.sogou.com/%s' % m.group(0)
+					print 'lrc file <%s>' % uri
+					cache = ClientCookie.urlopen(ClientCookie.Request(uri)).readlines()
+					lrc = []
+					for line in cache:
+						lrc.append(line.decode('gbk').encode('utf-8'))
+					lrc_content = parse_lyrics(lrc)
+					if verify_lyrics(lrc_content, artist, title):
+						lrc_path = '%s/%s - %s.lrc' % (LRCDIR, artist, title)
+						open(lrc_path, 'w').writelines(lrc)
+						return lrc_content
+			break
+	return {}
+
+class SogouLyrics(rb.Plugin):
+
+	def __init__(self):
+		rb.Plugin.__init__(self)
+
+	def elapsed_changed_handler(self, player, playing):
+		if playing:
+			elapsed = player.get_playing_time()
+			if self.lrc.has_key('offset'):
+				elapsed += self.lrc['offset']
+			try:
+				print '%d >> %s' % (elapsed, self.lrc[elapsed])
+				self.osd.send(MESSAGE_TEMPLATE % self.lrc[elapsed])
+			except KeyError:
+				pass
+		return
+
+	def playing_song_changed_handler(self, player, entry):
+		if entry is None:
+			return
+		# get playing song properties		
+		db = self.shell.get_property ('db')
+		artist = db.entry_get(entry, rhythmdb.PROP_ARTIST)
+		title = db.entry_get(entry, rhythmdb.PROP_TITLE)
+		print 'playing song changed to (%s - %s)' % (artist, title)
+		lrc_path = '%s/%s - %s.lrc' % (LRCDIR, artist, title)
+		# load lyrics content
+		self.lrc = {}
+		if os.path.exists(lrc_path) and os.path.isfile(lrc_path):
+			self.lrc = parse_lyrics(open(lrc_path, 'r').readlines())
+			if verify_lyrics(self.lrc, artist, title):
+				self.osd.send(MESSAGE_TEMPLATE % ('(%s - %s) prepared' % (artist, title)))
+			else:
+				self.lrc = {}
+				print 'broken lrc file %s moved to %s.bak' % (lrc_path, lrc_path)
+				try:
+					os.rename(lrc_path, '%s.bak' % lrc_path)
+				except OSError:
+					print 'move broken lrc file error'
+		if self.lrc == {}:
+			self.lrc = download_lyrics(artist, title)
+			if self.lrc == {}:
+				print 'download (%s - %s) failed' % (artist, title)
+				self.osd.send(MESSAGE_TEMPLATE % ('(%s - %s) not found' % (artist, title)))
+			else:
+				print 'download (%s - %s) success' % (artist, title)
+				self.osd.send(MESSAGE_TEMPLATE % ('(%s - %s) prepared' % (artist, title)))
+		return
+
+	def playing_changed_handler(self, player, playing):
+		# set player status		
+		self.playing = playing
+		print 'player status changed to %d' % playing
+		return
+
+	def activate(self, shell):
+		if not os.path.exists(LRCDIR):
+			os.mkdir(LRCDIR)
+		self.playing = 0
+		self.lrc = {}
+		self.player = shell.get_player()
+		self.shell = shell
+		self.handler = [
+			self.player.connect('playing-song-changed', self.playing_song_changed_handler),
+			self.player.connect('elapsed-changed', self.elapsed_changed_handler),
+			self.player.connect('playing-changed', self.playing_changed_handler)]
+		self.osd = eventbridge.OSD()
+		return
+
+	def deactivate(self, shell):
+		for handler in self.handler:
+			self.player.disconnect(handler)
+		del self.shell
+		del self.player
+		del self.lrc
+		del self.osd
+		return
+
+		
